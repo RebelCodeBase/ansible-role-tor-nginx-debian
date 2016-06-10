eafde8fa4ec4a1d60d014f4a332356721b4c198e
RebelCodeBase <rebelcode@riseup.net>
2016-06-10 02:12:52 +0200

    Ensure tor hidden service directory is created

--- spec/main.rb
+++ spec/main.rb
@@ -13,6 +13,13 @@ describe port 9050 do
   it { should be_listening.on('127.0.0.1').with('tcp') }
 end
 
+describe file '/var/lib/tor/hidden_service' do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by 'debian-tor' }
+  it { should be_grouped_into 'debian-tor' }
+end
+
 describe file '/var/lib/tor/hidden_service/private_key' do
   it { should exist }
   it { should be_file }
